76a9ddc1aea9d7175323b1cf2ba625f51ca4ee19
Ramon Oem <ramonoem@Ramons-MacBook-Pro-M4.local>
2026-01-23 17:07:10 +0700

    Optimize build: increase memory, disable sourcemaps, add webpack overrides

---
+++ config-overrides.js
@@ -0,0 +1,44 @@
+module.exports = function override(config) {
+  // Disable source maps to reduce memory usage
+  config.devtool = false;
+  
+  // Increase performance limits
+  config.performance = {
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000,
+    hints: false
+  };
+  
+  // Optimize chunks
+  if (config.optimization) {
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        vendor: {
+          name: 'vendor',
+          chunks: 'all',
+          test: /node_modules/,
+          priority: 20
+        },
+        tesseract: {
+          name: 'tesseract',
+          test: /[\\/]node_modules[\\/](tesseract\.js)[\\/]/,
+          chunks: 'all',
+          priority: 30,
+          reuseExistingChunk: true
+        },
+        common: {
+          name: 'common',
+          minChunks: 2,
+          chunks: 'all',
+          priority: 10,
+          reuseExistingChunk: true,
+          enforce: true
+        }
+      }
+    };
+  }
+  
+  return config;
+};
