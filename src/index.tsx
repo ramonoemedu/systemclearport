c63d69d836fb60b961ae94cf3c6dd5e0659b1b19
Ramon Oem <ramonoem@Ramons-MacBook-Pro-M4.local>
2026-01-23 17:17:20 +0700

    Fix: Remove unused imports from index.tsx to fix CI build errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
-import { onAuthStateChanged } from "firebase/auth";
 import { ThemeProviderWrapper } from './theme';
-
-import { auth } from "./firebase/config";
-import LoginPage from './components/Auth/LoginPage';
 import { App } from './App';
 
 
