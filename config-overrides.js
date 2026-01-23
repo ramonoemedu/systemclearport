458a31f9b73327e7e1ac20ab44849239b47b80f3
Ramon Oem <ramonoem@Ramons-MacBook-Pro-M4.local>
2026-01-23 17:09:38 +0700

    Fix: Increase ForkTsCheckerWebpackPlugin memory limit to 8GB

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-module.exports = function override(config) {
+module.exports = function override(config, env) {
   // Disable source maps to reduce memory usage
   config.devtool = false;
   
@@ -8,6 +8,18 @@ module.exports = function override(config) {
     maxEntrypointSize: 512000,
     hints: false
   };
+  
+  // Find and configure ForkTsCheckerWebpackPlugin
+  const ForkTsCheckerWebpackPlugin = config.plugins.find(
+    plugin => plugin.constructor.name === 'ForkTsCheckerWebpackPlugin'
+  );
+  
+  if (ForkTsCheckerWebpackPlugin) {
+    // Increase memory limit for TypeScript checker
+    ForkTsCheckerWebpackPlugin.options.memoryLimit = 8192;
+    // Make it less strict to avoid crashes
+    ForkTsCheckerWebpackPlugin.options.async = true;
+  }
   
   // Optimize chunks
   if (config.optimization) {
